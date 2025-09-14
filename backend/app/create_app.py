721242444f284a9dec39a2dce9085be57e7d0003
nlsnnn <egor.larrr@gmail.com>
2025-09-14 03:29:48 +0300

    add cors

--- backend/app/create_app.py
+++ backend/app/create_app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import register_routers, register_error_handlers
 
@@ -11,6 +12,17 @@ async def lifespan(app: FastAPI):
 
 def create_fastapi_app():
     app = FastAPI(lifespan=lifespan)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:5173",  # dev
+            "http://localhost",  # prod
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     register_routers(app)
     register_error_handlers(app)
